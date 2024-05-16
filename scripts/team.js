998593e5c4ec2bca8792e2db0b499bf1cbebb341
CharismaticTopHat <a0p5garcia@gmail.com>
2024-05-16 12:51:01 -0600

    Registro de Equipos - Datos Solicitados

--- scripts/team.js
+++ scripts/team.js
@@ -104,8 +104,87 @@ function playerSquares() {
         </tr>
         <tr>
           <td>
+            <br>
+          </td>
+        </tr>
+        <tr>
+        <tr>
+          <td>
             <p>En caso de ser menor de edad coloca el nombre completo de tu tutor legal (En caso de ser mayor de edad, responde NA)</p>
-            <input type="text" name="tutor">No</input>
+            <input type="text" name="tutor"></input>
+            <br>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <br>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p><strong>Soy mayor de edad</strong> y autorizo el uso de mi imagen en usos de multimedia para la difusión del evento, a través del Comité Organizador del Torneo de Fútbol 5 "Madriguera" </p>
+            <a href="https://drive.google.com/file/d/1YPWbkt8SNt5tcTi3ty6eJCwp7z4H4pJU/view?pli=1">*Consulta nuestro formato de uso de imagen para mayores de edad dando click en este enlace.</a>
+            <br>
+            <input type="radio" name="consent" value="true">Sí acepto</input>
+            <br>
+            <input type="radio" name="consent" value="false">No acepto</input>
+            <br>
+            <input type="radio" name="consent" value="false">Soy menor de edad</input>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <br>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>Soy madre, padre o tutor de un menor de edad y autorizo el uso de imagen de mi hijo/a en usos de multimedia para la difusión del evento, a través del Comité Organizador del Torneo de Fútbol 5 "La Madriguera" </p>
+            <a href="https://drive.google.com/file/d/1XaKUjhrQ_pD4izPLUyM_YAspJIuZ9Aun/view">*Consulta nuestro uso de imagen para menores de edad, dando click en este enlace.</a>
+            <br>
+            <input type="radio" name="consent" value="true">Sí acepto</input>
+            <br>
+            <input type="radio" name="consent" value="false">No acepto</input>
+            <br>
+            <input type="radio" name="consent" value="false">Soy menor de edad</input>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <br>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>Me comprometo a no ingerir bebidas alcohólicas ni estupefacientes en ningún espacio designado para La Liga de Fútbol 5 "Madriguera" </p>
+            <input type="radio" value="true">Acepto</input>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <br>
+          </td>
+        </tr>
+        <tr>
+        <tr>
+          <td>
+            <p>Deslindo y exonero de toda responsabilidad al Comité organizador de la Liga de Fútbol 5 "Madriguera" sus empleados, voluntarios, beneficiarios, consejeros, patrocinadores y demás relacionados al evento; de cualquier incidente. </p>
+            <input type="radio" value="true">Acepto</input>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <br>
+          </td>
+        </tr>
+        <tr>
+        <tr>
+          <td>
+            <p>Autorizo al Comité Organizador de la Liga de Fútbol 5 "Madriguera" o a quien designe a que en caso de que ocurra algún incidente durante LAS ACTIVIDADES se me brinde la atención necesaria, y en dado caso, el traslado a alguna Unidad Médica cercana, con la finalidad de que se atienda la emergencia y deslindo de toda responsabilidad al Comité Organizador de la Liga de Fútbol 5 "Madriguera"  y LOS COLABORADORES por las acciones aquí referidas o por las consecuencias inmediatas o futuras que se pudieran derivar de las mismas. </p>
+            <br>
+            <p>Acepto que es mi responsabilidad la certeza y suficiencia de la información médica entregada al Comité Organizador de la Liga de Fútbol 5 "Madriguera" que sea relevante, desde el momento de la inscripción y/o antes de participar en LAS ACTIVIDADES, y que esta información será proporcionada a las personas que me atiendan en caso de accidente.</p>
+            <br>
+            <input type="radio" value="true">Acepto</input>
           </td>
         </tr>
       </table>
