2d4c487ca3a327c6fd0ace87cae1339936666e15
CharismaticTopHat <a0p5garcia@gmail.com>
2024-05-19 02:31:53 -0600

    Registro de Equipos - Front End Realizado

--- scripts/team.js
+++ scripts/team.js
@@ -1,4 +1,4 @@
-function playerSquares() {
+function formSummon() {
   var selectedLiga = document.querySelector('input[name="liga"]:checked')
   if (!selectedLiga) {
     alert("Please select a league.");
@@ -6,99 +6,122 @@ function playerSquares() {
   }
   var modality = selectedLiga.value;
   var playersDiv = document.getElementById("players");
-  playersDiv.classList.add("fade-out");
-  setTimeout(() => {
-  if (modality == "LVDorada") {
+  if (modality != "") {
     playersDiv.innerHTML = `
       <table class="table-center">
       <tr>
-        <td>
-          <label for="email">Correo electrónico: </label>
-          <input type="text" name="email">
-        </td>
-      </tr>
-      <tr>
-        <td>
+        <td class="td-left">
           <label for="name">Nombre completo: </label>
           <input type="text" name="name">
         </td>
       </tr>
       <tr>
-        <td>
+        <td class="td-left">
+          <label for="email">Correo electrónico: </label>
+          <input type="text" name="email">
+        </td>S
+      </tr>
+      <tr>
+        <td class="td-left">
           <label for="age">Edad: </label>
           <input type="text" name="age">
         </td>
       </tr>
       <tr>
-        <td>
+        <td class="td-left">
           <label for="col">Colonia: </label>
           <input type="text" name="col">
         </td>
       </tr>
       <tr>
-        <td>
+        <td class="td-left">
           <label for="phoneNumber">Teléfono de Contacto (WhatsApp): </label>
           <input type="text" name="phoneNumber">
         </td>
       </tr>
       <tr>
-        <td>
+        <td class="td-left">
           <label for="teamName">Nombre del equipo: </label>
           <input type="text" name="teamName">
         </td>
       </tr>
       <tr>
         <td>
+         <br>
+        </td>
+      </tr>
+      <tr class="tr-strongyellow">
+        <td class="td-center">
           <label for="discovery">¿Cómo te enteraste de la Liga de Fútbol 5 "Madriguera"? </label>
           <br>
-          <div>
-            <input type="radio" name="medio" value="SNLeague">Redes Sociales de la Liga</input>
-            <br>
-            <input type="radio" name="medio" value="SNTopos">Redes Sociales de Topos FC</input>
-            <br>
-            <input type="radio" name="medio" value="invitation">Invitación Directa</input>
-            <br>
-            <input type="radio" name="medio" value="advertising">Publicidad Física</input>
-         </div>
+        </td>
+      </tr>
+      <tr class="tr-weakyellow">
+        <td>
+          <input type="radio" name="medio" value="SNLeague">Redes Sociales de la Liga</input>
+        </td>
+      </tr>
+      <tr class="tr-weakyellow">
+        <td>
+          <input type="radio" name="medio" value="SNTopos">Redes Sociales de Topos FC</input>
+        </td>
+      </tr>
+      <tr class="tr-weakyellow">
+        <td>
+          <input type="radio" name="medio" value="invitation">Invitación Directa</input>
+        </td>
+      </tr>
+      <tr class="tr-weakyellow">
+        <td>
+         <input type="radio" name="medio" value="advertising">Publicidad Física</input>
         </td>
       </tr>
       </table>
-      <br>
-      <table>
-        <tr>
-          <td>
-            <h3>Autorizaciones</h3>
+      <table class="table-center">
+        <tr class="tr-strongred">
+          <td class="td-center">
+            <h3 class="white-text">Autorizaciones</h3>
           </td>
         </tr>
-        <tr>
-          <td>
+        <tr class="tr-weakred">
+          <td class="td-center">
             <p><b>En esta sección aceptarás los términos y condiciones sobre tu participación en La Liga de Fútbol 5 "Madriguera"</b></p>
           </td>
         </tr>
-        <tr>
-          <td>
+        <tr class="tr-weakred">
+          <td class="td-center">
+          <br>
             <p><b>Por favor, lee todos los formatos presentes en las ligas de cada sección.</b></p>
           </td>
         </tr>
-        <tr>
-          <td>
+        <br>
+        <tr class="tr-weakred">
+          <td class="td-center">
             <p><b>Deberás entregar una identificación oficial vigente con copia</b></p>
           </td>
         </tr>
-        <tr>
-          <td>
+        <br>
+        <tr class="tr-weakred">
+          <td class="td-center">
+            <br>
             <p><strong>NOTA IMPORTANTE:</strong><b> En caso de ser menor de edad, deberás entregar copia de la identificación oficial vigente de tu tutor legal y de tu responsable durante el torneo, así como tu hoja responsiva impresa con la firma de tutor legal y de tu acompañante al evento (sólo en caso de que tu acompañante y tu tutor legal sean personas distintas)</b></p>
           </td>
         </tr>
       </table>
       <br>
-      <table>
-        <tr>
-          <td>
+      <table class="table-center">
+        <tr class="tr-strongyellow">
+          <td class="td-center">
             <label for="minAge">¿Eres mayor de edad? </label>
-            <br>
+          </td>
+        </tr>
+        <tr class="tr-weakyellow">
+          <td>
             <input type="radio" name="ageReq" value="true">Sí</input>
-            <br>
+          </td>
+        </tr>
+        <tr class="tr-weakyellow">
+          <td>
             <input type="radio" name="ageReq" value="false">No</input>
           </td>
         </tr>
@@ -108,11 +131,14 @@ function playerSquares() {
           </td>
         </tr>
         <tr>
-        <tr>
-          <td>
-            <p>En caso de ser menor de edad coloca el nombre completo de tu tutor legal (En caso de ser mayor de edad, responde NA)</p>
-            <input type="text" name="tutor"></input>
-            <br>
+        <tr class="tr-strongblue">
+          <td class="td-center">
+            <p class="white-text">En caso de ser menor de edad coloca el nombre completo de tu tutor legal (En caso de ser mayor de edad, responde NA)</p>
+          </td>
+        </tr>
+        <tr class="tr-weakblue">
+          <td class="td-center">
+            <input type="text" name="tutor" class="white-text"></input>
           </td>
         </tr>
         <tr>
@@ -120,15 +146,24 @@ function playerSquares() {
             <br>
           </td>
         </tr>
-        <tr>
-          <td>
+        <tr class="tr-strongyellow">
+          <td class="td-center">
             <p><strong>Soy mayor de edad</strong> y autorizo el uso de mi imagen en usos de multimedia para la difusión del evento, a través del Comité Organizador del Torneo de Fútbol 5 "Madriguera" </p>
-            <a href="https://drive.google.com/file/d/1YPWbkt8SNt5tcTi3ty6eJCwp7z4H4pJU/view?pli=1">*Consulta nuestro formato de uso de imagen para mayores de edad dando click en este enlace.</a>
-            <br>
-            <input type="radio" name="consent" value="true">Sí acepto</input>
-            <br>
+            <a href="https://drive.google.com/file/d/1YPWbkt8SNt5tcTi3ty6eJCwp7z4H4pJU/view?pli=1">*Consulta nuestro formato de uso de imagen para mayores de edad dando click en este enlace.</a> 
+          </td>
+        </tr>
+        <tr class="tr-weakyellow">
+          <td>
+          <input type="radio" name="consent" value="true">Sí acepto</input>
+          </td>
+        </tr>
+        <tr class="tr-weakyellow">
+          <td>
             <input type="radio" name="consent" value="false">No acepto</input>
-            <br>
+          </td>
+        </tr>
+        <tr class="tr-weakyellow">
+          <td>
             <input type="radio" name="consent" value="false">Soy menor de edad</input>
           </td>
         </tr>
@@ -137,15 +172,24 @@ function playerSquares() {
             <br>
           </td>
         </tr>
-        <tr>
+        <tr class="tr-strongblue">
+          <td class="td-center">
+            <p class="white-text">Soy madre, padre o tutor de un menor de edad y autorizo el uso de imagen de mi hijo/a en usos de multimedia para la difusión del evento, a través del Comité Organizador del Torneo de Fútbol 5 "La Madriguera" </p>
+            <a href="https://drive.google.com/file/d/1XaKUjhrQ_pD4izPLUyM_YAspJIuZ9Aun/view" class="white-text">*Consulta nuestro uso de imagen para menores de edad, dando click en este enlace.</a>
+          </td>
+        </tr>
+        <tr class="tr-weakblue">
           <td>
-            <p>Soy madre, padre o tutor de un menor de edad y autorizo el uso de imagen de mi hijo/a en usos de multimedia para la difusión del evento, a través del Comité Organizador del Torneo de Fútbol 5 "La Madriguera" </p>
-            <a href="https://drive.google.com/file/d/1XaKUjhrQ_pD4izPLUyM_YAspJIuZ9Aun/view">*Consulta nuestro uso de imagen para menores de edad, dando click en este enlace.</a>
-            <br>
             <input type="radio" name="consent" value="true">Sí acepto</input>
-            <br>
-            <input type="radio" name="consent" value="false">No acepto</input>
-            <br>
+          </td>
+        </tr>
+        <tr class="tr-weakblue">
+          <td>
+           <input type="radio" name="consent" value="false">No acepto</input>
+          </td>
+        </tr>
+        <tr class="tr-weakblue">
+          <td>
             <input type="radio" name="consent" value="false">Soy menor de edad</input>
           </td>
         </tr>
@@ -154,9 +198,29 @@ function playerSquares() {
             <br>
           </td>
         </tr>
-        <tr>
-          <td>
+        <tr class="tr-strongyellow">
+          <td class="td-center">
             <p>Me comprometo a no ingerir bebidas alcohólicas ni estupefacientes en ningún espacio designado para La Liga de Fútbol 5 "Madriguera" </p>
+          </td>
+        </tr>
+        <tr class="tr-weakyellow">
+          <td>
+           <input type="radio" value="true">Acepto</input>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <br>
+          </td>
+        </tr>
+        <tr>
+        <tr class="tr-strongblue">
+          <td class="td-center">
+            <p class="white-text">Deslindo y exonero de toda responsabilidad al Comité organizador de la Liga de Fútbol 5 "Madriguera" sus empleados, voluntarios, beneficiarios, consejeros, patrocinadores y demás relacionados al evento; de cualquier incidente. </p>
+          </td>
+        </tr>
+        <tr class="tr-weakblue">
+          <td>
             <input type="radio" value="true">Acepto</input>
           </td>
         </tr>
@@ -166,181 +230,30 @@ function playerSquares() {
           </td>
         </tr>
         <tr>
-        <tr>
-          <td>
-            <p>Deslindo y exonero de toda responsabilidad al Comité organizador de la Liga de Fútbol 5 "Madriguera" sus empleados, voluntarios, beneficiarios, consejeros, patrocinadores y demás relacionados al evento; de cualquier incidente. </p>
-            <input type="radio" value="true">Acepto</input>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <br>
-          </td>
-        </tr>
-        <tr>
-        <tr>
-          <td>
+        <tr class="tr-strongyellow">
+          <td class="td-center">
             <p>Autorizo al Comité Organizador de la Liga de Fútbol 5 "Madriguera" o a quien designe a que en caso de que ocurra algún incidente durante LAS ACTIVIDADES se me brinde la atención necesaria, y en dado caso, el traslado a alguna Unidad Médica cercana, con la finalidad de que se atienda la emergencia y deslindo de toda responsabilidad al Comité Organizador de la Liga de Fútbol 5 "Madriguera"  y LOS COLABORADORES por las acciones aquí referidas o por las consecuencias inmediatas o futuras que se pudieran derivar de las mismas. </p>
             <br>
             <p>Acepto que es mi responsabilidad la certeza y suficiencia de la información médica entregada al Comité Organizador de la Liga de Fútbol 5 "Madriguera" que sea relevante, desde el momento de la inscripción y/o antes de participar en LAS ACTIVIDADES, y que esta información será proporcionada a las personas que me atiendan en caso de accidente.</p>
-            <br>
-            <input type="radio" value="true">Acepto</input>
+          </td>
+        </tr>
+        <tr class="tr-weakyellow">
+          <td class="td-center">
+          <input type="radio" value="true">Acepto</input>
           </td>
         </tr>
       </table>
+      <br>
+    <div>
+      <button type="button">
+        <strong>Registrar equipo</strong>
+      </button>
+    </div> 
       `;    
-  } else if (modality == "LVEstrella") {
-    playersDiv.innerHTML = `
-    <table class="table-center">
-      <tr>
-        <td class="td-center" colspan="4">
-            <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-        </td>
-        <td class="td-center" colspan="4">
-            <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-        </td>
-        <td class="td-center" colspan="4">
-            <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-        </td>
-        <td class="td-center" colspan="4">
-            <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-        </td>
-       </tr>
-       <tr>
-          <td class="td-center" colspan="4">
-              <input placeholder='Nombre completo'/>
-          </td>
-          <td class="td-center" colspan="4">
-            <input placeholder='Nombre completo'/>
-          </td>
-          <td class="td-center" colspan="4">
-              <input placeholder='Nombre completo'/>
-          </td>
-          <td class="td-center" colspan="4">
-              <input placeholder='Nombre completo'/>
-          </td>
-        </tr>
-        <tr>
-          <td class="td-filler"></td>
-          <td class="td-filler"></td>
-        </tr>
-       <tr>
-          <td class="td-filler"></td>
-          <td class="td-center" colspan="4">
-              <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/> 
-          </td>
-          <td class="td-filler"></td>
-          <td class="td-center" colspan="4">
-              <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-          </td>
-          <td class="td-filler"></td>
-          <td class="td-center" colspan="4">
-              <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-          </td>
-          <td class="td-filler"></td>
-       </tr>
-       <tr>
-          <td class="td-filler"></td>
-          <td class="td-center" colspan="4">
-            <input placeholder='Nombre completo' />
-          </td>
-          <td class="td-filler"></td>
-          <td class="td-center" colspan="4">
-            <input placeholder='Nombre completo' />
-          </td>
-          <td class="td-filler"></td>
-          <td class="td-center" colspan="4">
-            <input placeholder='Nombre completo' />
-          </td>
-          <td class="td-filler"></td>
-          <td class="td-filler"></td>
-       </tr>
-      </table>`;
-  } else if (modality == "LFTalpa") {
-    playersDiv.innerHTML = `
-        <table class="table-center">
-        <tr>
-        <td class="td-center" colspan="3">
-            <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-        </td>
-        <td class="td-center" colspan="3">
-            <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-        </td>
-        <td class="td-center" colspan="3">
-            <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-        </td>
-       </tr>
-       <tr>
-          <td class="td-center" colspan="3">
-              <input placeholder='Nombre completo'/>
-          </td>
-          <td class="td-center" colspan="3">
-            <input placeholder='Nombre completo'/>
-          </td>
-          <td class="td-center" colspan="3">
-              <input placeholder='Nombre completo'/>
-          </td>
-        </tr>
-        <tr>
-          <td class="td-filler"></td>
-        </tr>
-           <tr>
-            <td class="td-center" colspan="3">
-                <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-            </td>
-            <td class="td-center" colspan="3">
-                <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-            </td>
-            <td class="td-center" colspan="3">
-                <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-            </td>
-           </tr>
-           <tr>
-              <td class="td-center" colspan="3">
-                  <input placeholder='Nombre completo'/>
-              </td>
-              <td class="td-center" colspan="3">
-                <input placeholder='Nombre completo'/>
-              </td>
-              <td class="td-center" colspan="3">
-                  <input placeholder='Nombre completo'/>
-              </td>
-            </tr>
-              <tr>
-            <td class="td-filler"></td>
-          </tr>
-            <tr>
-            <td class="td-center" colspan="3">
-                <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-            </td>
-            <td class="td-center" colspan="3">
-                <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-            </td>
-            <td class="td-center" colspan="3">
-                <img src="images/default_user.png" alt="Imagen de usuario no registrado" class="ima_usuario"/>
-            </td>
-           </tr>
-           <tr>
-              <td class="td-center" colspan="3">
-                  <input placeholder='Nombre completo'/>
-              </td>
-              <td class="td-center" colspan="3">
-                <input placeholder='Nombre completo'/>
-              </td>
-              <td class="td-center" colspan="3">
-                  <input placeholder='Nombre completo'/>
-              </td>
-            </tr>`;
   } else {
     playersDiv.innerHTML = `
       <h3>
         <p>Escoja una de las opciones en el cuadro de opciones anterior.</p>
       </h3>`;
   }
-  playersDiv.classList.remove("fade-in");
-  playersDiv.classList.add("fade-out");
-  setTimeout(() => {
-    playersDiv.classList.remove("fade-out");
-  }, 300);
-  }, 300);
 }
