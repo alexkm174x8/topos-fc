444985fe1f9fa34eb168c3337a1ec914a503d34b
Alvaro <alvaro.a.cruz.j@gmail.com>
2024-06-07 01:56:47 -0600

    Ãbase de datos corregida de estadisticas,  marcadores, logos

--- prueba.php
+++ prueba.php
@@ -4,21 +4,32 @@ $username = "root";
 $password = "";
 $dbname = "topos";
 
-// Crear la conexión
+$nombre_html = isset($_GET['nombre_html']) ? $_GET['nombre_html'] : '';
+
+switch ($nombre_html) {
+    case 'estadistica_varonil':
+        $idLiga = 1;
+        break;
+    case 'estadistica_femenil':
+        $idLiga = 3;
+        break;
+    case 'estadistica_topitos':
+        $idLiga = 2;
+        break;
+    default:
+        $idLiga = 1;
+}
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
-
-$nombre_html = isset($_GET['nombre_html']) ? $_GET['nombre_html'] : '';
 
 if (isset($_GET['equipo'])) {
     $equipo = $conn->real_escape_string($_GET['equipo']);
     $sql = "SELECT * FROM TOPOS_Equipo WHERE nombre='$equipo'";
     $result = $conn->query($sql);
-
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         $response = json_encode($row);
@@ -26,30 +37,24 @@ if (isset($_GET['equipo'])) {
         $response = json_encode([]);
     }
     echo $response;
-} else if (isset($_GET['marcador'])) {
-    $sql = "SELECT marcador_casa, marcador_visita FROM topos_partido ORDER BY fecha DESC LIMIT 1";
+} elseif (isset($_GET['marcador'])) {
+    $sql = "SELECT marcador_casa, marcador_visita, equipo_casa.logo AS logo_casa, equipo_visita.logo AS logo_visita FROM TOPOS_Partido
+            INNER JOIN TOPOS_Equipo AS equipo_casa ON TOPOS_Partido.equipo_casa = equipo_casa.idEquipo
+            INNER JOIN TOPOS_Equipo AS equipo_visita ON TOPOS_Partido.equipo_visita = equipo_visita.idEquipo
+            WHERE TOPOS_Partido.idLiga = $idLiga ORDER BY fecha DESC LIMIT 1";
     $result = $conn->query($sql);
     $marcador = [];
 
     if ($result->num_rows > 0) {
         $marcador = $result->fetch_assoc();
     }
-
     echo json_encode($marcador);
 } else {
-    $sql = "SELECT nombre, logo FROM TOPOS_Equipo";
-    if ($nombre_html === 'estadistica_varonil') {
-        $sql .= " WHERE IDLIGA = 1";
-    } elseif ($nombre_html === 'estadistica_femenil') {
-        $sql .= " WHERE IDLIGA = 2";
-    } elseif ($nombre_html === 'estadistica_topitos') {
-        $sql .= " WHERE IDLIGA = 3";
-    }
+    $sql = "SELECT nombre, logo FROM TOPOS_Equipo WHERE idLiga = $idLiga";
     $result = $conn->query($sql);
     $equipos = [];
-
     if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
+        while ($row = $result->fetch_assoc()) {
             $equipos[] = $row;
         }
     }
--- scripts/estadisticas-script.js
+++ scripts/estadisticas-script.js
@@ -1,28 +1,83 @@
+document.addEventListener('DOMContentLoaded', function() {
+    mostrarMarcadores();
+    cargarEquipos();
+    cargarDatosLiga();
+});
+
 function toggleDropdown() {
     document.getElementById("dropdown-content").classList.toggle("show");
     document.getElementById("team-details").innerHTML = ""; // Limpiar los detalles del equipo
     cargarEquipos();
 }
 
-function mostrarMarcadores() {
-    fetch('prueba.php?marcador=true')
+function cargarDatosLiga() {
+    const nombreHtml = document.body.getAttribute('data-page-name');
+    fetch(`prueba.php?nombre_html=${nombreHtml}`)
         .then(response => response.json())
         .then(data => {
+            console.log('Datos recibidos (liga):', data);
+            // Verificamos si se recibieron los datos de la liga
+            if (data && data.length > 0) {
+                // Actualizamos los valores de partidos y goles
+                document.getElementById('partidos').innerHTML = `<h1>${data[0].partidos_totales}</h1><br><h3>PARTIDOS</h3>`;
+                document.getElementById('goles').innerHTML = `<h1>${data[0].goles_totales}</h1><br><br><h3>GOLES</h3>`;
+            } else {
+                console.error('No se encontraron datos de la liga.');
+            }
+        })
+        .catch(error => console.error('Error en cargarDatosLiga:', error));
+}
+
+
+function cargarEscudos(escudoIzquierda, escudoDerecha) {
+    const escudoIzquierdaEl = document.getElementById('escudo_izquierda');
+    const escudoDerechaEl = document.getElementById('escudo_derecha');
+
+    // Comprobamos si se proporcionaron ambos escudos
+    if (escudoIzquierda && escudoDerecha) {
+        escudoIzquierdaEl.src = escudoIzquierda;
+        escudoDerechaEl.src = escudoDerecha;
+    } else {
+        // Si al menos uno de los escudos no está presente, se muestra un mensaje de error
+        console.error('Al menos uno de los escudos no está presente.');
+
+        // Verificamos individualmente si cada escudo está presente y lo asignamos si es así
+        if (escudoIzquierda) {
+            escudoIzquierdaEl.src = escudoIzquierda;
+        }
+        if (escudoDerecha) {
+            escudoDerechaEl.src = escudoDerecha;
+        }
+    }
+}
+
+
+function mostrarMarcadores() {
+    const nombreHtml = document.body.getAttribute('data-page-name');
+    fetch(`prueba.php?nombre_html=${nombreHtml}&marcador=true`)
+        .then(response => response.json())
+        .then(data => {
+            console.log('Datos recibidos (marcadores):', data);
             const marcadoresDiv = document.querySelector('.marcadores h1');
-            if (data) {
+            if (data && data.marcador_casa !== undefined && data.marcador_visita !== undefined) {
                 marcadoresDiv.textContent = `${data.marcador_casa} - ${data.marcador_visita}`;
+                cargarEscudos(data.logo_casa, data.logo_visita);
             } else {
                 marcadoresDiv.textContent = "No se encontraron marcadores.";
             }
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => console.error('Error en mostrarMarcadores:', error));
 }
+
+
+
 
 function cargarEquipos() {
     const nombreHtml = document.body.getAttribute('data-page-name');
     fetch(`prueba.php?nombre_html=${nombreHtml}`)
         .then(response => response.json())
         .then(data => {
+            console.log('Datos recibidos (equipos):', data);  // Verifica los datos recibidos
             const dropdownContent = document.getElementById('dropdown-content');
             dropdownContent.innerHTML = '';
             data.forEach(equipo => {
@@ -38,15 +93,16 @@ function cargarEquipos() {
                 dropdownContent.appendChild(button);
             });
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => console.error('Error en cargarEquipos:', error));
 }
 
 function handleTeamButtonClick(equipo) {
     fetch(`prueba.php?equipo=${equipo}`)
         .then(response => response.json())
         .then(data => {
+            console.log('Datos recibidos (equipo):', data);  // Verifica los datos recibidos
             const teamDetails = document.getElementById('team-details');
-            if (data) {
+            if (data && Object.keys(data).length > 0) {
                 teamDetails.innerHTML = `
                     <h2 style="margin-top:5vh;">${data.nombre}</h2>
                     <table style="border-collapse: collapse; border: 5px solid #000; width: 100%; margin-top: 5vh;">
@@ -82,10 +138,5 @@ function handleTeamButtonClick(equipo) {
                 teamDetails.innerHTML = '<p>No se encontraron detalles del equipo.</p>';
             }
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => console.error('Error en handleTeamButtonClick:', error));
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    cargarEquipos();
-    mostrarMarcadores()
-});
