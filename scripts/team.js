537e8e265de186fad88854b24ceffbf149f8cc9d
CharismaticTopHat <a0p5garcia@gmail.com>
2024-06-07 18:46:18 -0600

    Team Register - Fixed Format

--- scripts/team.js
+++ scripts/team.js
@@ -92,7 +92,6 @@ window.onload = function() {
 function formSummon() {
   var selectedLiga = document.querySelector('input[name="idLiga"]:checked');
   if (!selectedLiga) {
-      alert("Por favor escoja una liga.");
       return;
   }
   var modality = selectedLiga.value;
@@ -104,99 +103,93 @@ function formSummon() {
       <input type="hidden" name="teamId" id="teamId" value="">
       <table class="table-center">
       <tr>
-        <td class="td-left">
+        <td class="td-center">
           <label for="names">Nombre/s: </label>
           <input type="text" name="name" required>
         </td>
       </tr>
       <tr>
-        <td class="td-left">
+        <td class="td-center">
           <label for="lastNames">Apellidos: </label>
           <input type="text" name="lastNames" required>
         </td>
       </tr>
       <tr>
-        <td class="td-left">
+        <td class="td-center">
           <label for="email">Correo electrónico: </label>
           <input type="email" name="email" required>
         </td>
       </tr>
       <tr>
-        <td class="td-left">
+        <td class="td-center">
           <label for="age">Edad: </label>
           <input type="number" name="age" required>
+          <br>
         </td>
       </tr>
       <tr>
-        <td class="td-left">
+        <td class="td-center">
           <label for="col">Colonia: </label>
           <input type="text" name="col" required>
         </td>
       </tr>
-      <tr>
-        <td class="td-left">
+        <td class="td-center">
           <label for="phoneNumber">Teléfono de Contacto (WhatsApp): </label>
           <input type="number" name="phoneNumber" required>
         </td>
       </tr>
       <tr>
-        <td class="td-left">
+        <td class="td-center">
           <label for="teamName">Nombre del equipo: </label>
           <input type="text" name="teamName" required>
         </td>
       </tr>
       <tr>
-        <td>
-         <br>
-        </td>
-      </tr>
-      <tr>
-        <td class="td-left">
+        <td class="td-center">
           <label for="teamLogo">URL/Enlace del logo del equipo: </label>
           <input type="text" name="teamLogo" required>
         </td>
       </tr>
       <tr>
         <td>
-         <br>
+          <br>
         </td>
-      </tr>
+      <tr>
       <tr class="strongyellow">
         <td class="td-center">
           <label for="discovery">¿Cómo te enteraste de la Liga de Fútbol 5 "Madriguera"? </label>
-          <br>
         </td>
       </tr>
       <tr class="weakyellow">
-        <td>
+        <td class="td-center">
           <input type="radio" name="medio" value="Redes sociales de la liga" required>Redes Sociales de la Liga</input>
         </td>
       </tr>
       <tr class="weakyellow">
-        <td>
+        <td class="td-center">
           <input type="radio" name="medio" value="Redes Sociales de Topos">Redes Sociales de Topos FC</input>
         </td>
       </tr>
       <tr class="weakyellow">
-        <td>
+        <td class="td-center">
           <input type="radio" name="medio" value="Invitación directa">Invitación Directa</input>
         </td>
       </tr>
       <tr class="weakyellow">
-        <td>
+        <td class="td-center">
          <input type="radio" name="medio" value="Publicidad física">Publicidad Física</input>
         </td>
       </tr>
       <tr class="weakyellow">
-        <td>
+        <td class="td-center">
          <input type="radio" name="medio" value="Otros">Otros</input>
         </td>
       </tr>
       </table>
       <table class="table-center">
         <tr class="strongred">
-          <td class="td-center">
-            <h3 class="white-text">Autorizaciones</h3>
+          <td class="td-left">
+            <p class="white-text"><strong>Autorizaciones</strong></p>
           </td>
         </tr>
         <tr class="weakred">
@@ -232,12 +225,12 @@ function formSummon() {
           </td>
         </tr>
         <tr class="weakyellow">
-          <td>
+          <td class="td-center">
             <input type="radio" name="ageReq" value="Sí" required>Sí</input>
           </td>
         </tr>
         <tr class="weakyellow">
-          <td>
+          <td class="td-center">
             <input type="radio" name="ageReq" value="No">No</input>
           </td>
         </tr>
@@ -265,16 +258,16 @@ function formSummon() {
         <tr class="strongyellow">
           <td class="td-center">
             <p><strong>Soy mayor de edad</strong> y autorizo el uso de mi imagen en usos de multimedia para la difusión del evento, a través del Comité Organizador del Torneo de Fútbol 5 "Madriguera" </p>
-            <a href="https://drive.google.com/file/d/1YPWbkt8SNt5tcTi3ty6eJCwp7z4H4pJU/view?pli=1">*Consulta nuestro formato de uso de imagen para mayores de edad dando click en este enlace.</a> 
+            <a href="https://drive.google.com/file/d/1YPWbkt8SNt5tcTi3ty6eJCwp7z4H4pJU/view?pli=1" class="custom-link">*Consulta nuestro formato de uso de imagen para mayores de edad dando click en este enlace.</a> 
           </td>
         </tr>
         <tr class="weakyellow">
-          <td>
-          <input type="radio" name="consent" value="Aceptado" required>Sí acepto</input>
+          <td class="td-center">
+            <input type="radio" name="consent" value="Aceptado" required>Sí acepto</input>
           </td>
         </tr>
         <tr class="weakyellow">
-          <td>
+          <td class="td-center">
             <input type="radio" name="consent" value="No aceptado">No acepto</input>
           </td>
         </tr>
@@ -285,22 +278,22 @@ function formSummon() {
         </tr>
         <tr class="strongblue">
           <td class="td-center">
-            <p class="white-text">En caso de ser menor de edad autorizo el uso de la imagen de mi hijo (a) en usos de multimedia para la difusión del evento, a través del Comité Organizador del Torneo de Fútbol 5 "Madriguera".</p>
-            <a href="https://drive.google.com/file/d/1YPWbkt8SNt5tcTi3ty6eJCwp7z4H4pJU/view?pli=1">*Consulta nuestro formato de uso de imagen para menores de edad dando click en este enlace.</a>
+            <p class="white-text">Soy madre, padre o tutor de un menor de edad autorizo el uso de la imagen de mi hijo (a) en usos de multimedia para la difusión del evento, a través del Comité Organizador del Torneo de Fútbol 5 "Madriguera".</p>
+            <a href="https://drive.google.com/file/d/1YPWbkt8SNt5tcTi3ty6eJCwp7z4H4pJU/view?pli=1" class="custom-link">*Consulta nuestro formato de uso de imagen para menores de edad dando click en este enlace.</a>
           </td>
         </tr>
         <tr class="weakblue">
-          <td>
+          <td class="td-center">
             <input type="radio" name="parentConsent" value="Aceptado" required>Sí acepto</input>
           </td>
         </tr>
         <tr class="weakblue">
-          <td>
+          <td <td class="td-center">
            <input type="radio" name="parentConsent" value="No aceptado">No acepto</input>
           </td>
         </tr>
         <tr class="weakblue">
-          <td>
+          <td <td class="td-center">
             <input type="radio" name="parentConsent" value="Menor de edad">Soy menor de edad</input>
           </td>
         </tr>
@@ -315,7 +308,7 @@ function formSummon() {
           </td>
         </tr>
         <tr class="weakyellow">
-          <td>
+          <td <td class="td-center">
            <input type="radio" name="noAlcohol" value="true" required>Acepto</input>
           </td>
         </tr>
@@ -331,7 +324,7 @@ function formSummon() {
           </td>
         </tr>
         <tr class="weakblue">
-          <td>
+          <td <td <td class="td-center">
             <input type="radio" name="responsabilityRelease" value="true" required>Acepto</input>
           </td>
         </tr>
